ac42f1a1c85eb1247622f796e0d45989723c2e2f
romanulanov <turnsourcream@gmail.com>
2026-02-25 15:23:48 +0400

    add typing, main_tasks_run., main_tasks_stop

--- task_queue_workers.py
+++ task_queue_workers.py
@@ -13,58 +13,79 @@
 - Логгер должен записывать в файл: `{timestamp} | {number} -> {result}`    
 - Реализовать graceful shutdown всех потоков (корректное завершение работы)    
 **Цель:** Понять взаимодействие потоков через очереди, thread-safe структуры, координацию потоков.'''
+
 from queue import Queue
 from random import randint, uniform
 from time import time, sleep
-from threading import Event
+from threading import Thread, Event
+from typing import List
 
-q1 = Queue()
-q2 = Queue()
+q1: Queue = Queue()
+q2: Queue = Queue()
 stop_event = Event()
 
-def task_generator(work_time:int = 10):
+
+def task_generator():
     start_time = time()
     while True:
+        elapsed = int((time() - start_time))
         if elapsed >= 10 or stop_event.is_set():
             break
         q1.put(randint(1, 100))
         sleep(0.5)
-        elapsed = int((time.time() - start_time))
     q1.put(None)
-    q1.shutdown()
-    #TODO остановка из-за статуса событий Event
-    
+    stop_event.set()
+
 
 def task_logger():
+    with open("file.txt", "w", encoding="utf-8"):
+        pass
     while True:
         task = q2.get()
         if not task or stop_event.is_set():
+            stop_event.set()
             break
         with open("file.txt", "a", encoding="utf-8") as f:
-            f.write(task)
+            f.write(f"{str(task)}\n")
 
 
-def task_worker(worker_id: int):
+def task_worker():
     while True:
         task = q1.get()
-        q1.task_done()
         if not task or stop_event.is_set():
+            q2.put(None)
+            q1.task_done()
+            stop_event.set()
             break
         q2.put(task**2)
+        q1.task_done()
         sleep(uniform(0.1, 0.5))
 
 
 def print_info():
-    #TODO прочитать файл и вывести статистику
+    with open("file.txt", "r", encoding="utf-8") as file:
+        print(file.read())
 
 
-def main_tasks_run():
-    #TODO запустить task_generator, task_logger и task_worker
+def main_tasks_run(threads: List[Thread]):
+    for thread in threads:
+        thread.start()
 
 
-def main_tasks_stop():
-    #TODO передаёт сигнал стоп task_generator и дождаться его заверщения (join)
-    #TODO передаёт сигнал стоп task_worker и дождаться его заверщения (join)
-    #TODO передаёт сигнал стоп task_logger и дождаться его заверщения (join)
-    #TODO дождаться завершения задачи в очередях и дождаться его заверщения (join)
-    #TODO вывести статистику (print_info)
+def main_tasks_stop(threads: List[Thread]):
+    for thread in threads:
+        thread.join()
+    print_info()
+
+
+def main():
+    thread1 = Thread(target=task_generator)
+    thread2 = Thread(target=task_logger)
+    thread3 = Thread(target=task_worker)
+    threads = [thread1, thread2, thread3]
+    main_tasks_run(threads)
+    main_tasks_stop(threads)
+
+
+if __name__ == '__main__':
+    main()
