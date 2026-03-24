8726520ef9b616b1dd82a6b470d6c842ebc60b8b
romanulanov <turnsourcream@gmail.com>
2026-03-24 16:00:32 +0400

    refactor: rename to handler, add broadcast, rooms & message history

--- server.py
+++ server.py
@@ -1,31 +1,50 @@
 import asyncio
 
-from json import loads
+from json import dumps, loads
 from websockets.asyncio.server import serve
 
 
-rooms: dict[str, set] = {}
+rooms: dict[str, dict] = {}
 for num in range(1, 6):
-    rooms[f"room{num}"] = set()
+    rooms[f"room{num}"] = {
+        "users": set(),
+        "messages": list(),
+        }
 
 
-async def hello(websocket):
+async def broadcast(room_id, message, exclude=None):
+    for client in rooms[room_id]["users"]:
+        if client != exclude:
+            await client.send(message)
+
+
+async def handler(websocket):
     try:
         user = await websocket.recv()
         user = loads(user)
-        print(f"<<< На сервер заходит {user['name']}")
-        greeting = f"Привет {user['name']} из комнаты {user['room']}!"
+        user, room_id = user['name'], user['room']
+        print(f"<<< На сервер заходит {user}")
+        greeting = f"Привет {user} из комнаты {room_id}!"
+        if rooms[room_id]["messages"]:
+            for message in rooms[room_id]["messages"]:
+                await websocket.send(message)
+        if len(rooms[room_id]["users"]) > 1:
+            await broadcast(room_id, f"{user} теперь с нами!")
         await websocket.send(greeting)
         print(f">>> {greeting}")
+        rooms[room_id]["users"].add(websocket)
         async for message in websocket:
-            print(f"Сообщение в {user['room']} от {user['name']}: {message}")
-
+            await broadcast(room_id, f"{user} пишет: {message}", exclude=websocket)
+            rooms[room_id]["messages"].append(f"{user} написал: {message}")
     except Exception as e:
         print(f"Error: {e}")
+    finally:
+        await broadcast(room_id, f"{user} вышел!", exclude=websocket)
+        rooms[room_id]["users"].remove(websocket)
 
 
 async def main():
-    async with serve(hello, "127.0.0.1", 8765) as server:
+    async with serve(handler, "127.0.0.1", 8765) as server:
         print("Запущен сервер ws://127.0.0.1:8765")
         await server.serve_forever()
 
